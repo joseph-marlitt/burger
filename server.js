f5091e87e8b64ccac38ec2ca1518d4a5c88a1722
Joseph-Marlitt <joseph.marlitt@gmail.com>
2018-02-13 16:38:37 -0600

    draft

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
-var MethodOverride = require("method-override");
+var methodOverride = require("method-override");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ defaultLayout: "index" }));
 app.set("view engine", "handlebars");
 
-app.use(express.static("app/public"));
+app.use(methodOverride('X-HTTP-Method-Override'));
 
-require("./app/routes/api-routes.js")(app);
-
+var routes = require("./controllers/burger_controller.js");
+app.use(routes);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
