5a8562a538f752388e366a711e48dda74ab5c7e8
Joseph-Marlitt <joseph.marlitt@gmail.com>
2018-02-13 13:38:09 -0600

    draft

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,10 @@
-DROP DATABASE IF EXISTS `burgers_db`;
-CREATE DATABASE `burgers_db`;
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
-CREATE TABLE `chirps` (
-	`id` Int( 11 ) AUTO_INCREMENT NOT NULL,
-  	`burger_name` VARCHAR( 255) NOT NULL,
-  	`devoured` BOOLEAN DEFAULT NOT NULL,
+CREATE TABLE burgers (
+	id Int(11) AUTO_INCREMENT NOT NULL,
+  	burger_name VARCHAR(255) NOT NULL,
+  	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
