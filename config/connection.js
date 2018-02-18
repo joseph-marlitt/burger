5bf792228d7b63f30f671788ddc7ca939e2b2d75
Joseph-Marlitt <joseph.marlitt@gmail.com>
2018-02-18 13:29:17 -0600

    draft

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 var mysql = require("mysql");
 
 // Set up our connection information
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   port: 8889,
   host: "localhost",
   user: "root",
   password: "root",
   database: "burgers_db"
-});
+  });
+};
 
 // Connect to the database
 connection.connect(function(err) {
